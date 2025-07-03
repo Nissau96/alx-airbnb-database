a4fbdaab33583d505707b9672ef2269d3fde190f
Nissua96 <ihalidu996@gmail.com>
2025-07-03 17:38:36 +0000

    Updated database_index file

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,51 @@
+EXPLAIN ANALYZE
+SELECT 
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date,
+    bookings.total_price,
+    bookings.status,
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    users.email
+FROM bookings
+INNER JOIN users ON bookings.user_id = users.user_id;
+
+-- Measuring performance of LEFT JOIN query
+EXPLAIN ANALYZE
+SELECT 
+    properties.property_id,
+    properties.name AS property_name,
+    properties.location,
+    properties.pricepernight,
+    reviews.review_id,
+    reviews.rating,
+    reviews.comment,
+    reviews.created_at AS review_date
+FROM properties 
+LEFT JOIN reviews ON properties.property_id = reviews.property_id
+ORDER BY properties.property_id;
+
+-- Measuring performance of FULL OUTER JOIN query
+EXPLAIN ANALYZE
+SELECT 
+    users.user_id,
+    users.first_name,
+    users.last_name,
+    bookings.booking_id,
+    bookings.property_id,
+    bookings.start_date,
+    bookings.end_date,
+    bookings.total_price,
+    bookings.status
+FROM users
+FULL OUTER JOIN bookings ON users.user_id = bookings.user_id;
+
+
+
+
 -- Index for booking status filtering
 CREATE INDEX idx_bookings_status ON bookings(status);
 
